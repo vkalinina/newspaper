7bbec5f1db551f004e94b68673e6d4589215aa0a
Viktoria Kalinina <viktoriakalinina@orderberry.de>
2025-11-18 07:26:47 +0100

    add: configure URLs for catalog app

---
+++ catalog/urls.py
@@ -0,0 +1,75 @@
+from django.urls import path
+
+from .views import (
+    index,
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+    RedactorListView,
+    RedactorDetailView,
+    RedactorCreateView,
+    RedactorExperienceUpdateView,
+    RedactorDeleteView,
+    TopicListView,
+    TopicCreateView,
+    TopicUpdateView,
+    TopicDeleteView,
+    toggle_assign_to_articles,
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+    path(
+        "topics/",
+        TopicListView.as_view(),
+        name="topic-list",
+    ),
+    path(
+        "topics/create/",
+        TopicCreateView.as_view(),
+        name="topic-create",
+    ),
+    path(
+        "topics/<int:pk>/update/",
+        TopicUpdateView.as_view(),
+        name="topic-update",
+    ),
+    path(
+        "topics/<int:pk>/delete/",
+        TopicDeleteView.as_view(),
+        name="topic-delete",
+    ),
+    path("articles/", ArticleListView.as_view(), name="article-list"),
+    path("articles/<int:pk>/", ArticleDetailView.as_view(), name="article-detail"),
+    path("articles/create/", ArticleCreateView.as_view(), name="article-create"),
+    path("articles/<int:pk>/update/", ArticleUpdateView.as_view(), name="article-update"),
+    path("articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name="article-delete"),
+    path(
+        "articles/<int:pk>/toggle-assign/",
+        toggle_assign_to_articles,
+        name="toggle-article-assign",
+    ),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path(
+        "redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"
+    ),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path(
+        "redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"
+    ),
+    path("redactors/create/", RedactorCreateView.as_view(), name="redactor-create"),
+    path(
+        "redactors/<int:pk>/update/",
+        RedactorExperienceUpdateView.as_view(),
+        name="redactor-update",
+    ),
+    path(
+        "redactors/<int:pk>/delete/",
+        RedactorDeleteView.as_view(),
+        name="redactor-delete",
+    ),
+]
+
+app_name = "catalog"
