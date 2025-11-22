da2014f1c51e1b35321e37652bed428d8dfb7bc6
Viktoria Kalinina <viktoriakalinina@orderberry.de>
2025-11-22 16:20:26 +0100

    feat: add endpoint for reductor update

--- catalog/urls.py
+++ catalog/urls.py
@@ -42,10 +42,18 @@ urlpatterns = [
         name="topic-delete",
     ),
     path("articles/", ArticleListView.as_view(), name="article-list"),
-    path("articles/<int:pk>/", ArticleDetailView.as_view(), name="article-detail"),
-    path("articles/create/", ArticleCreateView.as_view(), name="article-create"),
-    path("articles/<int:pk>/update/", ArticleUpdateView.as_view(), name="article-update"),
-    path("articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name="article-delete"),
+    path(
+        "articles/<int:pk>/", ArticleDetailView.as_view(), name="article-detail"
+    ),
+    path(
+        "articles/create/", ArticleCreateView.as_view(), name="article-create"
+    ),
+    path(
+        "articles/<int:pk>/update/", ArticleUpdateView.as_view(), name="article-update"
+    ),
+    path(
+        "articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name="article-delete"
+    ),
     path(
         "articles/<int:pk>/toggle-assign/",
         toggle_assign_to_articles,
