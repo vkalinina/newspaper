ae30390cce8bbdde52327028d5af99b031255f13
Viktoria Kalinina <viktoriakalinina@orderberry.de>
2025-11-28 14:10:29 +0100

    chore: ready for deploy - split settings, PostgreSQL config

--- newspaper/settings/dev.py
+++ newspaper/settings/dev.py
@@ -6,6 +6,16 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
